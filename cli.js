caf40e0304a6e33e1fd263d3201925c5103752fc
Kristoffer Walker <kris@kixx.name>
2016-09-15 17:07:56 -0400

    Successfully making API requests
    	new file:   bin/cli
    	new file:   cli.js
    	new file:   lib/client.js
    	new file:   lib/oddworks-client.js
    	modified:   package.json

---
+++ cli.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const url = require('url');
+const Promise = require('bluebird');
+const U = require('lodash');
+const yargs = require('yargs');
+const filepath = require('filepath');
+const Client = require('./lib/client');
+const OddworksClient = require('./lib/oddworks-client');
+
+exports.main = function main() {
+	const args = yargs
+		.usage('Usage: $0 [options]')
+		.option('config', {
+			demand: true,
+			describe: 'Path to the JSON config file'
+		})
+		.help();
+
+	const argv = args.argv;
+
+	return loadConfigs(argv.config)
+		.then(config => {
+			config = config || {};
+
+			if (!config.jwtSecret) {
+				return Promise.reject(new Error('Config is missing jwtSecret'));
+			}
+			if (!config.jwtIssuer) {
+				return Promise.reject(new Error('Config is missing jwtIssuer'));
+			}
+			if (!config.channel) {
+				return Promise.reject(new Error('Config is missing channel'));
+			}
+			if (!config.baseUrl) {
+				return Promise.reject(new Error('Config is missing baseUrl'));
+			}
+			if (!U.isString(config.pathPrefix)) {
+				return Promise.reject(new Error('Config is missing pathPrefix'));
+			}
+			if (!config.authHeader) {
+				return Promise.reject(new Error('Config is missing authHeader'));
+			}
+			if (!Array.isArray(config.platforms)) {
+				return Promise.reject(new Error('Config is missing platforms'));
+			}
+
+			let baseUrl;
+			try {
+				baseUrl = url.parse(config.baseUrl);
+			} catch (err) {
+				const uriError = new Error(`Config baseUrl is invalid: ${err.message}`);
+				return Promise.reject(uriError);
+			}
+
+			return config.platforms.map(spec => {
+				if (!spec.id) {
+					throw new Error('Config platform is missing an ID');
+				}
+				if (!Array.isArray(spec.paths)) {
+					return Promise.reject(new Error('Config platform is missing a paths Array'));
+				}
+
+				return new Client({
+					jwtSecret: config.jwtSecret,
+					jwtIssuer: config.jwtIssuer,
+					channel: config.channel,
+					platform: spec.id,
+					paths: spec.paths,
+					oddworksClient: new OddworksClient({
+						protocol: baseUrl.protocol.replace(/:$/, ''),
+						host: baseUrl.hostname,
+						port: baseUrl.port,
+						pathPrefix: config.pathPrefix,
+						authHeader: config.authHeader
+					})
+				});
+			});
+		})
+		.then(clients => {
+			const promises = clients.map(client => {
+				return client.authenticate();
+			});
+
+			return Promise.all(promises);
+		})
+		.then(clients => {
+			const client = clients[0];
+			return client.makeNextRequest();
+		});
+};
+
+function loadConfigs(path) {
+	const file = filepath.create(path);
+	if (!file.isFile()) {
+		throw new Error(`Config path is not a file "${path}"`);
+	}
+
+	return file.read().then(text => {
+		let config;
+		try {
+			config = JSON.parse(text);
+		} catch (err) {
+			const jsonError = new Error(`JSON config file parsing error: ${err.message}`);
+			return Promise.reject(jsonError);
+		}
+		return config;
+	});
+}
